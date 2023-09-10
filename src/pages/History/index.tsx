6a6240037f3f39e599ec88869e3584a1b1a919c9
Inacio Silva <joseinaciolds@gmail.com>
2023-09-10 12:32:21 -0300

    usando a bibliotecan react-hook-from e monipulando o formulário

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,78 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 Minutos</td>
+            <td>Há dois messes</td>
+            <td>
+              <Status statusColor="green">Concluido</Status>
+            </td>
+          </tr>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
